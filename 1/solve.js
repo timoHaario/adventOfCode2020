e7dc9921a4eb0dac36fa1e41d7a7ae805b41b4ad
Timo Haario <timo.haario@futurice.com>
2020-12-03 08:42:54 +0200

    add comments

--- 1/solve.js
+++ 1/solve.js
@@ -2,7 +2,7 @@ const { solve } = require("../utils");
 
 const path = "./input.txt";
 
-const solvePartOneBruteForce = (input) => {
+const partOne = (input) => {
   loop: for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length; j++) {
       const sum = Number(input[i]) + Number(input[j]);
@@ -14,7 +14,7 @@ const solvePartOneBruteForce = (input) => {
   }
 };
 
-const solvePartTwoBruteForce = (input) => {
+const partTwo = (input) => {
   loop: for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length; j++) {
       for (let k = 0; k < input.length; k++) {
@@ -28,6 +28,8 @@ const solvePartTwoBruteForce = (input) => {
   }
 };
 
-solve(path, solvePartOneBruteForce, solvePartTwoBruteForce);
-// function parametes are optional, so you can as well run solve with only one function
-// solve(path, solvePartOneBruteForce);
+solve(path, partOne, partTwo);
+/*
+  function parametes are optional, so you can as well run solve with only one function
+  solve(path, partOne);
+*/
